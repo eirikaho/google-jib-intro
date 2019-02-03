2072df478c331f0ff55e00b51cdfd4e7b46b7d73
Eirik Holst <eirik.holst@bouvet.no>
2019-02-03 16:37:38 +0100

    init commit

---
+++ src/main/java/no/bouvet/googlejibintro/GoogleJibIntroApplication.java
@@ -0,0 +1,14 @@
+package no.bouvet.googlejibintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleJibIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleJibIntroApplication.class, args);
+	}
+
+}
+
